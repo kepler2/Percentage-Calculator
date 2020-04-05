f15b9fe4287bae8031d71616220a78a350418488
kepler2 <dubfire08@gmail.com>
2020-04-05 19:12:30 +0300

    Added icons
    Added fa icons

--- main.js
+++ main.js
@@ -2,15 +2,6 @@ const value1 = document.querySelector('#box1');
 const value2 = document.querySelector('#box2');
 const result1 = document.querySelector('#result1');
 
-
-// document.addEventListener("keyup", function(e){
-    
-//     {
-//         calculate();
-//     }
-// })
-
-
 // Delete key function
 document.addEventListener("keyup", function(e){
     if (e.keyCode == 46)
@@ -30,9 +21,8 @@ document.addEventListener("keyup", function(e){
     }
 })
 
-
+// Event listener for Calculate button
 document.querySelector('#calculate1').addEventListener("click", calculate);
-
 
 function calculate(){
     result = parseFloat(value1.value / 100 * value2.value);
